1da6a5bcc1754845d176f2f489c8e972c8218be3
lucyoz <lucyoz.kwon@gmail.com>
2022-11-17 14:39:40 +0900

    [UPDATE] addUser() 수정 - username 중복체크(없다면 save, 있다면 msg 띄움)

--- src/main/java/com/example/springbootjpa/service/UserService.java
+++ src/main/java/com/example/springbootjpa/service/UserService.java
@@ -34,7 +34,15 @@ public class UserService {
         //dto를 entity로
         User user = userRequest.toEntity();
 
-        User savedUser = userRepository.save(user);
-        return new UserResponse(savedUser.getId(), savedUser.getUsername(), savedUser.getPassword());
+        //저장하기 전 username으로 select해봅니다.
+        //있으면 중복되었습니다 메세지로 알려주고 .save하지 않음
+        Optional<User> selectedUser = userRepository.findByUsername(userRequest.getUsername());
+        if(selectedUser.isPresent()){
+            return new UserResponse(null, userRequest.getUsername(), "이 user는 이미 존재합니다. 다른 이름을 사용합니다.");
+        }else{
+            User savedUser = userRepository.save(user);
+            return new UserResponse(savedUser.getId(), savedUser.getUsername(), savedUser.getPassword());
+        }
+
     }
 }
