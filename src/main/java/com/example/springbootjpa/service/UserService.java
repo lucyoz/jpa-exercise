661aa8d9165e23b8784ce6b279fccd2617ce2bc3
lucyoz <lucyoz.kwon@gmail.com>
2022-11-17 13:40:54 +0900

    [ADD] UserService에 getUser()메소드

---
+++ src/main/java/com/example/springbootjpa/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.springbootjpa.service;
+
+import com.example.springbootjpa.model.User;
+import com.example.springbootjpa.model.dto.UserDto;
+import com.example.springbootjpa.model.dto.UserResponse;
+import com.example.springbootjpa.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserResponse getUser(Long id){
+        Optional<User> optUser = userRepository.findById(id);
+
+        if (optUser.isPresent()){
+            User user = optUser.get();
+            return new UserResponse(user.getId(), user.getUsername(), "");
+        }else{
+            return new UserResponse(id,"","해당 id의 유저가 없습니다.");
+        }
+
+    }
+}
