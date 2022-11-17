44c8553ecf03c7df079e14b51ca17c2316c7bf2a
lucyoz <lucyoz.kwon@gmail.com>
2022-11-17 14:26:33 +0900

    [UPDATE] UserRequest에 toEntity() 추가  - UserService에서 사용위해  - dto를 Entity로 만들기

--- src/main/java/com/example/springbootjpa/model/dto/UserRequest.java
+++ src/main/java/com/example/springbootjpa/model/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package com.example.springbootjpa.model.dto;
 
+import com.example.springbootjpa.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,4 +13,13 @@ import lombok.NoArgsConstructor;
 public class UserRequest {
     private String username;
     private String password;
+
+    public User toEntity(){
+        User user = User.builder()
+                .username(this.username)
+                .password(this.password)
+                .build();
+
+        return user;
+    }
 }
