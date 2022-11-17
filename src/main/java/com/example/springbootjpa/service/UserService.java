da25467b9ba2861c4cbb9cbbb0af5127b3a9bb95
lucyoz <lucyoz.kwon@gmail.com>
2022-11-17 14:27:17 +0900

    [UPDATE] UserService에 addUser()  - 사용자 등록

--- src/main/java/com/example/springbootjpa/service/UserService.java
+++ src/main/java/com/example/springbootjpa/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.springbootjpa.service;
 
 import com.example.springbootjpa.model.User;
 import com.example.springbootjpa.model.dto.UserDto;
+import com.example.springbootjpa.model.dto.UserRequest;
 import com.example.springbootjpa.model.dto.UserResponse;
 import com.example.springbootjpa.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -27,5 +28,13 @@ public class UserService {
             return new UserResponse(id,"","해당 id의 유저가 없습니다.");
         }
 
+    }
+
+    public UserResponse addUser(UserRequest userRequest){
+        //dto를 entity로
+        User user = userRequest.toEntity();
+
+        User savedUser = userRepository.save(user);
+        return new UserResponse(savedUser.getId(), savedUser.getUsername(), savedUser.getPassword());
     }
 }
